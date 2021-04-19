5a1d13511c54b0f9a7a6a8c63d00e8b37cdb5ece
An Yi <anyi@MacBook-Pro.local>
2021-04-18 21:42:11 -0700

    db.js schema for profile page

--- back-end/db.js
+++ back-end/db.js
@@ -3,12 +3,23 @@ const mongoose = require('mongoose')
 
 // user schema
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true }
-    
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    aboutMe: { type: String, required: false },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 })
 
 // recipe schema
-
 const User = mongoose.model('User',userSchema)
+
+
+// generate password hash
+User.methods.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+}
 
 module.exports = User;
